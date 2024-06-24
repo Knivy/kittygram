6401fac058d9aa410146110fcefaadd1d863d71e
Albina Giliazova <giliazova@mail.ru>
2024-06-24 20:11:26 +0500

    final

--- cats/views.py
+++ cats/views.py
@@ -1,19 +1,123 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# @api_view(['GET', 'POST'])
+# def cat_list(request):
+#     if request.method == 'POST':
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     cats = Cat.objects.all()
+#     serializer = CatSerializer(cats, many=True)
+#     return Response(serializer.data)
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# class APICat(APIView):
+#     def get(self, request):
+#         cats = Cat.objects.all()
+#         serializer = CatSerializer(cats, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED) 
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+# from rest_framework import generics
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer 
+
+from rest_framework import viewsets 
+
+from .models import Cat, Owner
+from .serializers import CatSerializer, OwnerSerializer, CatListSerializer
+
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .models import Cat
-from .serializers import CatSerializer
+from rest_framework import mixins
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    # В теле класса никакой код не нужен! Пустячок, а приятно.
+    pass 
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer 
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer 
+
+    def get_serializer_class(self):
+        # Если запрошенное действие (action) — получение списка объектов ('list')
+        if self.action == 'list':
+            # ...то применяем CatListSerializer
+            return CatListSerializer
+        # А если запрошенное действие — не 'list', применяем CatSerializer
+        return CatSerializer 
+
+    # Пишем метод, а в декораторе разрешим работу со списком объектов
+    # и переопределим URL на более презентабельный
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        # Нужно получить записи о пяти котиках белого цвета
+        cats = Cat.objects.filter(color='White')[:5]
+        # Передадим queryset cats сериализатору 
+        # и разрешим работу со списком объектов
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data) 
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer 
+# from rest_framework import viewsets
+# from rest_framework.response import Response
+
+# from django.shortcuts import get_object_or_404
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# class CatViewSet(viewsets.ViewSet):
+#     def list(self, request):
+#         queryset = Cat.objects.all()
+#         serializer = CatSerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+#     def retrieve(self, request, pk=None):
+#         queryset = Cat.objects.all()
+#         cat = get_object_or_404(queryset, pk=pk)
+#         serializer = CatSerializer(cat)
+#         return Response(serializer.data) 
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,69 @@
-from django.urls import path
+# from django.urls import path
 
-from cats.views import cat_list
+# from cats.views import cat_list
+
+# urlpatterns = [
+#    path('cats/', cat_list),
+# ]
+
+# from cats.views import APICat
+
+# urlpatterns = [
+#     path('cats/', APICat.as_view()),
+# ] 
+
+# from cats.views import CatList, CatDetail
+
+# urlpatterns = [
+#     path('cats/', CatList.as_view()),
+#     path('cats/<int:pk>/', CatDetail.as_view()),
+# ] 
+
+# from cats.views import CatViewSet
+
+# urlpatterns = [
+#     path('cats/', CatViewSet.as_view({'get': 'list'})),
+# ] 
+# from rest_framework.routers import SimpleRouter
+
+# from django.urls import include, path
+
+# from cats.views import CatViewSet
+
+# # Создаётся роутер
+# router = SimpleRouter()
+# # Вызываем метод .register с нужными параметрами
+# router.register('cats', CatViewSet)
+# # В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# # например
+# # router.register('owners', OwnerViewSet)
+# # Но нам это пока не нужно
+
+# urlpatterns = [
+#     # Все зарегистрированные в router пути доступны в router.urls
+#     # Включим их в головной urls.py
+#     path('', include(router.urls)),
+# ] 
+
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
+from rest_framework.authtoken import views
+
+
+router = DefaultRouter()
+router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
-   path('cats/', cat_list),
+    path('', include(router.urls)),
+    # path('api-token-auth/', views.obtain_auth_token),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
-
-
